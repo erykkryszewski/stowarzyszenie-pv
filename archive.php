bbae285563b36492815c9c1276f1537a64d04a3b
Eryk Kryszewski <eric9972@o2.pl>
2025-08-27 09:55:49 +0200

    site progress

--- archive.php
+++ archive.php
@@ -1,115 +1,124 @@
-<?php 
-
+<?php
 get_header();
-global $post;
+global $post, $wp_query;
 
-// Get the current page number
-$current_blog_page = get_query_var('paged') ? get_query_var('paged') : 1;
+// bieżąca strona dla paginacji
+$current_blog_page = get_query_var('paged') ? (int) get_query_var('paged') : 1;
 
-$args = array( 
-  'post_type' => 'kursy', 
-  'post_status' => 'publish',
-  'posts_per_page' => 12,  
-  'orderby' => 'title',
-  'paged' => $current_blog_page
-);
-
-$global_logo = get_field('global_logo', 'options');
+// tytuł archiwum w tej samej logice co wcześniej
 $archive_title = get_the_archive_title();
 $title_parts = explode(':', $archive_title);
 $page_title = wp_strip_all_tags(end($title_parts));
-
-$query = new WP_Query($args);
-
 ?>
-
-<main id="main" class="main <?php if(!is_front_page()) { echo 'main--subpage'; }?>">
+<main id="main" class="main <?php if (!is_front_page()) {
+  echo 'main--subpage';
+} ?>">
   <div class="subpage-hero">
     <div class="subpage-hero__background subpage-hero__background--plain"></div>
     <div class="container">
       <div class="subpage-hero__wrapper">
-        <h1 class="subpage-hero__title"><?php echo apply_filters('the_title', $page_title);?></h1>
+        <h1 class="subpage-hero__title"><?php echo apply_filters('the_title', $page_title); ?></h1>
       </div>
     </div>
   </div>
-  <div class="spacer" style="height: 90px"></div>
+
+  <div class="spacer" style="height: 75px"></div>
+
   <div class="section-title">
     <div class="container">
       <div class="section-title__wrapper section-title__wrapper--decorated">
-        <h2>Lorem ipsum dolor sit amet.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptas amet reiciendis consequuntur praesentium est laudantium iure veniam voluptatem maxime! </p>
+        <h2 class="section-title__title section-title__title--left"><span>Lorem ipsum</span>Dolor sit amet consectetur.
+        </h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur accusantium explicabo odit sunt ipsum
+          voluptas animi, ratione error sequi similique?</p>
       </div>
     </div>
   </div>
-  <?php if($query->have_posts()):?>
-    <div class="theme-blog theme-blog--courses">
-      <div class="container">
-        <div class="theme-blog__wrapper theme-blog__wrapper--courses">
-          <div class="row">
-            <?php while ($query->have_posts()) : $query->the_post(); ?>
-              <div class="col-12 col-md-6 col-lg-4 theme-blog__column theme-blog__column--courses">
-                <div class="theme-blog__item theme-blog__item--courses">
-                  <div class="theme-blog__image theme-blog__image--courses">
-                    <a href="<?php the_permalink();?>" class="cover"></a>
-                    <?php echo wp_get_attachment_image(get_post_thumbnail_id(), 'full', '', ["class" => "object-fit-contain"]); ?>
-                  </div>
-                  <div class="theme-blog__content theme-blog__content--courses">
-                    <div>
-                      <a href="<?php the_permalink(); ?>" class="theme-blog__title"><?php the_title(); ?></a>
-                      <?php
-                      $excerpt = get_the_excerpt();
-                      $content = get_the_content();
 
-                      if (!empty($excerpt)) {
-                        echo '<p>' . mb_substr($excerpt, 0, 150) . (mb_strlen($excerpt) > 150 ? '...' : '') . '</p>';
-                      } elseif (empty($excerpt) && !empty($content)) {
-                        $contentText = strip_tags($content);
-                        echo '<p>' . mb_substr($contentText, 0, 150) . (mb_strlen($contentText) > 150 ? '...' : '') . '</p>';
-                      }
-                      
-                    ?>
-                    </div>
-                      <a href="<?php the_permalink(); ?>" class="theme-blog__button button"><?php _e('Czytaj więcej', 'ercodingtheme'); ?></a>
-                  </div>
-                </div>
-              </div>
-            <?php endwhile; ?>
-          </div>
-        </div>
-        <div class="pagination mt-5">
-          <?php
-            echo paginate_links(array(
-              'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-              'current'      => max(1, get_query_var('paged')),
-              'format'       => '?paged=%#%',
-              'total'        => $query->max_num_pages, // Use max_num_pages from the custom query
-              'show_all'     => false,
-              'type'         => 'list',
-              'end_size'     => 2,
-              'mid_size'     => 1,
-              'prev_next'    => true,
-              'prev_text'    => '',
-              'next_text'    => '',
-              'add_args'     => false,
-              'add_fragment' => '',
-            ));
-          ?>
-        </div>
-        <?php wp_reset_postdata(); ?>
-      </div>
-    </div>
-  <?php endif;?>
-  <div class="spacer spacer--small" style="height: 40px"></div>
-  <div class="cta">
+  <div class="spacer" style="height: 50px"></div>
+
+  <?php if (have_posts()): ?>
+  <div class="theme-blog">
     <div class="container">
-      <div class="cta__wrapper">
-        <h2 class="cta__title">Zapoznaj się z naszą ofertą!</h2>
-        <p></p>
-        <div>
-          <a href="/kontakt/" class="cta__button button">Skontaktuj się</a>
+      <div class="theme-blog__wrapper">
+        <div class="row">
+          <?php while (have_posts()):
+
+            the_post();
+
+            $terms = get_the_terms(get_the_ID(), 'category') ?: [];
+            $mods = array_map(fn($t) => 'theme-blog__item--' . sanitize_html_class($t->slug), $terms);
+            $theme_blog_item = 'theme-blog__item' . (!empty($mods) ? ' ' . implode(' ', $mods) : '');
+
+            $is_video = false;
+            if (!empty($terms)) {
+              foreach ($terms as $term) {
+                if ($term->slug === 'video') {
+                  $is_video = true;
+                  break;
+                }
+              }
+            }
+            ?>
+          <div class="col-12 col-md-6 col-lg-4 theme-blog__column">
+            <div class="<?php echo esc_attr($theme_blog_item); ?>">
+              <div class="theme-blog__image <?php echo $is_video ? 'theme-blog__image--video' : ''; ?>">
+                <a href="<?php the_permalink(); ?>" class="cover"></a>
+                <?php echo wp_get_attachment_image(get_post_thumbnail_id(), 'full', false, [
+                  'class' => 'object-fit-cover',
+                  'loading' => 'lazy',
+                  'decoding' => 'async',
+                ]); ?>
+              </div>
+              <div class="theme-blog__content">
+                <div>
+                  <a href="<?php the_permalink(); ?>" class="theme-blog__title"><?php the_title(); ?></a>
+                  <p class="small">
+                    <?php
+                    $excerpt = get_the_excerpt();
+                    if (empty($excerpt)) {
+                      $content = get_the_content();
+                      echo esc_html(wp_trim_words(wp_strip_all_tags($content), 15, '…'));
+                    } else {
+                      echo esc_html(wp_trim_words($excerpt, 15, '…'));
+                    }
+                    ?>
+                  </p>
+                </div>
+                <a href="<?php the_permalink(); ?>" class="theme-blog__button">
+                  <?php if ($is_video) {
+                    _e('Zobacz video', 'ercodingtheme');
+                  } else {
+                    _e('Czytaj więcej', 'ercodingtheme');
+                  } ?>
+                </a>
+              </div>
+            </div>
+          </div>
+          <?php
+          endwhile; ?>
         </div>
+      </div>
+
+      <div class="pagination mt-5">
+        <?php echo paginate_links([
+          'base' => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+          'current' => max(1, $current_blog_page),
+          'format' => '?paged=%#%',
+          'total' => (int) $wp_query->max_num_pages,
+          'show_all' => false,
+          'type' => 'list',
+          'end_size' => 2,
+          'mid_size' => 1,
+          'prev_next' => true,
+          'prev_text' => '',
+          'next_text' => '',
+          'add_args' => false,
+          'add_fragment' => '',
+        ]); ?>
       </div>
     </div>
   </div>
+  <?php endif; ?>
 </main>
 <?php get_footer(); ?>
--- js/src/index.js
+++ js/src/index.js
@@ -11,17 +11,22 @@ import '@fancyapps/fancybox';
 
 // AOS.init();
 
-$(window).on('load', function() {
+$(window).on('load', function () {
   // AOS.refresh();
 
   setTimeout(() => {
     $('.preloader').fadeOut(250);
   }, 900);
-  
-  if($('.products__elements').length > 0 && $(window).width() < 768) {
-    $('html, body').delay(200).animate({
-      scrollTop: $('.products__elements').offset().top
-    }, 1000);
+
+  if ($('.products__elements').length > 0 && $(window).width() < 768) {
+    $('html, body')
+      .delay(200)
+      .animate(
+        {
+          scrollTop: $('.products__elements').offset().top,
+        },
+        1000,
+      );
   }
 });
 
@@ -29,34 +34,37 @@ $(document).on('click', 'a[href^="#"]', function (event) {
   event.preventDefault();
 
   if (window.location.href.indexOf('produkt') === -1 && window.location.href.indexOf('product') === -1) {
-    $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 650);
+    $('html, body').animate(
+      {
+        scrollTop: $($.attr(this, 'href')).offset().top,
+      },
+      650,
+    );
   }
 });
 
-$(document).ready(function() {
-  $('img[sizes^="auto,"]').each(function() {
+$(document).ready(function () {
+  $('img[sizes^="auto,"]').each(function () {
     var $img = $(this);
     var sizes = $img.attr('sizes').replace(/^auto,\s*/, '');
     $img.attr('sizes', sizes);
   });
 });
 
-$(document).ready(function($) {
-  $("img[title]").removeAttr("title");
+$(document).ready(function ($) {
+  $('img[title]').removeAttr('title');
   $('p:empty').remove();
 });
 
 // empty pages redirection
 
-document.addEventListener("DOMContentLoaded", function () {
-  let mainElement = document.querySelector("main#main");
+// document.addEventListener("DOMContentLoaded", function () {
+//   let mainElement = document.querySelector("main#main");
 
-  if (mainElement && mainElement.innerHTML.trim() === "") {
-    window.location.href = "/";
-  }
-});
+//   if (mainElement && mainElement.innerHTML.trim() === "") {
+//     window.location.href = "/";
+//   }
+// });
 
 /* imports */
 
@@ -93,4 +101,3 @@ import './woocommerce/single-product';
 import './woocommerce/content-product';
 import './woocommerce/cart';
 import './woocommerce/checkout';
-
